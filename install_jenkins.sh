27c3570f896ddadf1c36e80543765f51f696a7db
silky2001 <62842645+silky2001@users.noreply.github.com>
2021-08-04 15:57:33 +0530

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sudo yum update –y
-sudo yum install docker* -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
